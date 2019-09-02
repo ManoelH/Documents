9ce22514d5e394cc395aea4881fb76911765fce8
manoel <henriqe115@gmail.com>
2019-09-02 16:34:39 -0300

    update documents

--- src/main/java/com/springproject/documents/controller/DocumentController.java
+++ src/main/java/com/springproject/documents/controller/DocumentController.java
@@ -1,11 +1,14 @@
 package com.springproject.documents.controller;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -19,12 +22,14 @@ import com.springproject.documents.repository.Documents;
 @RequestMapping("/documents")
 public class DocumentController {
 
+	private static final String REGISTER_DOCUMENT_VIEW = "RegisterDocument";
+	
 	@Autowired
 	private Documents documents;
 	
 	@RequestMapping("/new")
 	public ModelAndView newDocument() {
-		ModelAndView modelAndView = new ModelAndView("RegisterDocument");
+		ModelAndView modelAndView = new ModelAndView(REGISTER_DOCUMENT_VIEW);
 		modelAndView.addObject("allStatus", StatusDocument.values());
 		modelAndView.addObject(new Document());
 		return modelAndView;
@@ -42,11 +47,18 @@ public class DocumentController {
 	@RequestMapping(method = RequestMethod.POST)
 	public String save(@Validated Document document, Errors erros, RedirectAttributes attributes) {
 		if(erros.hasErrors())
-			return "RegisterDocument";
+			return REGISTER_DOCUMENT_VIEW;
 		 
 		attributes.addFlashAttribute("message", "Document saved with success!");
 		documents.save(document);
 		return "redirect:/documents/new";
+	}
+	
+	@RequestMapping("/{id}")
+	public ModelAndView put(@PathVariable("id") Document document) {
+		ModelAndView modelAndView = new ModelAndView(REGISTER_DOCUMENT_VIEW);
+		modelAndView.addObject(document);
+		return modelAndView;
 	}
 	
 }
